3035dd8f1598878b0dd7a6a0f5daa76c24616168
Ben Falk <falk.ben@gmail.com>
2017-09-14 16:13:47 -0400

    for gen_commands.py, each "worker" command that is generated is now aligned to the supercuboid z (16).  Also, more verbose about options

--- gen_commands.py
+++ gen_commands.py
@@ -2,20 +2,23 @@
 
 import shlex
 
-""" Script to generate ingest commands for multiple processes """
+""" Script to generate ingest commands for ingest program """
+""" Once generated, copy commands to terminal and run them """
 
-# Recommend copy this to a new location for editing
+""" Recommend copy this to a new location for editing """
 
 
 script = "ingest_large_vol.py"
 
 source_type = 's3'  # either 'local' or 's3'
-s3_bucket_name = "BUCKET_NAME"  # not needed for 'local' source_type
-aws_profile = "default"  # not needed for 'local' source_type
+s3_bucket_name = "BUCKET_NAME"  # not used for 'local' source_type
+aws_profile = "default"  # not used for 'local' source_type
 
 boss_config_file = "neurodata.cfg"  # location on local system for boss API key
 
-# Slack messages (optional but recommended)
+# Slack messages (optional)
+# sends a slack message when ingest is finished with a link to the see the data
+# set to a blank string (e.g. '') to exclude from command output
 slack_token = "slack_token"  # Slack token for sending Slack messages
 slack_username = "SLACKUSER"  # your slack username
 
@@ -24,17 +27,21 @@ collection = 'COLL'
 experiment = 'EXP'
 channel = 'Ch1'
 
-# data_directory _with_ trailing slash
+# data_directory _with_ trailing slash (doesn't output correct paths on Windows)
 data_directory = "DATA_DIR/"
+
+# filename without extension (no '.tif')
+# <p:4> indicates the z index of the tif file, with up to 4 leading zeros
 file_name = "FILENAME<p:4>"
+
+# extension name for images, supported image types are PNG and TIFF
+# extension just needs to match the filename and can be any string (e.g.: ome, tif, png)
+file_format = 'tif'
 
 # increment of filename numbering (always increment in steps of 1 in the boss, typically will be '1')
 z_step = '1'
 
-# extension name for images, supported image types are PNG and TIFF
-file_format = 'tif'
-
-# float or int
+# float or int supported
 voxel_size = 'XXX YYY ZZZ'
 
 # nanometers/micrometers/millimeters/centimeters
@@ -46,11 +53,46 @@ data_type = 'uint16'
 # pixel extent for images in x, y and number of total z slices
 data_dimensions = "XXXX YYYY ZZZZ"
 
-# first inclusive, last _exclusive_ list of sections to ingest, integers, typically the same as ZZZZ "data_dimensions"
+# first inclusive, last _exclusive_ list of sections to ingest
+# integers, typically the same as ZZZZ "data_dimensions"
 zrange = [0, ZZZZ]
 
-# Number of workers to use, watch for out of memory errors
+# Number of workers to use
+# each worker loads additional 16 image files so watch out for out of memory errors
 workers = 6
+
+
+""" Code to generate the commands """
+
+
+def gen_comm(zstart, zend):
+    cmd = "python " + script + " "
+    cmd += ' --base_path ' + shlex.quote(data_directory)
+    cmd += ' --base_filename ' + shlex.quote(file_name)
+    cmd += ' --extension ' + file_format
+    cmd += ' --datasource ' + source_type
+    cmd += ' --collection ' + collection
+    cmd += ' --experiment ' + experiment
+    cmd += ' --channel ' + channel
+    cmd += ' --voxel_size ' + voxel_size
+    cmd += ' --voxel_unit ' + voxel_unit
+    cmd += ' --datatype ' + data_type
+    cmd += ' --img_size ' + data_dimensions
+    cmd += ' --z_range %d %d ' % (zstart, zend)
+    cmd += ' --z_step ' + z_step
+    cmd += ' --warn_missing_files'
+    cmd += ' --boss_config_file ' + boss_config_file
+
+    if slack_token != '' and slack_username != '':
+        cmd += ' --slack_token_file ' + slack_token
+        cmd += " --slack_usr " + slack_username
+
+    if source_type == 's3':
+        cmd += " --s3_bucket_name " + s3_bucket_name
+        cmd += ' --aws_profile ' + aws_profile
+    cmd += " &"
+
+    return cmd
 
 
 range_per_worker = (zrange[1] - zrange[0]) // workers
@@ -71,65 +113,28 @@ elif data_type == 'uint16':
 elif data_type == 'uint64':
     mult = 8
 mem_per_w = ddim_xy[0] * ddim_xy[1] * mult * 16 / 1024 / 1024 / 1024
-print('Expected memory usage per worker {:.1f} GB'.format(mem_per_w))
+print('# Expected memory usage per worker {:.1f} GB'.format(mem_per_w))
 
 # amount of memory total
 mem_tot = mem_per_w * workers
-print('Expected total memory usage: {:.1f} GB'.format(mem_tot))
+print('# Expected total memory usage: {:.1f} GB'.format(mem_tot))
 
 
-cmd = "python " + script + " "
-cmd += ' --base_path ' + shlex.quote(data_directory)
-cmd += ' --base_filename ' + shlex.quote(file_name)
-cmd += ' --extension ' + file_format
-cmd += ' --datasource ' + source_type
-cmd += ' --collection ' + collection
-cmd += ' --experiment ' + experiment
-cmd += ' --channel ' + channel
-cmd += ' --voxel_size ' + voxel_size
-cmd += ' --voxel_unit ' + voxel_unit
-cmd += ' --datatype ' + data_type
-cmd += ' --img_size ' + data_dimensions
-cmd += ' --z_range ' + ' '.join(list(map(str, zrange)))
-cmd += ' --boss_config_file ' + boss_config_file
-cmd += ' --slack_token_file ' + slack_token
-if source_type == 's3':
-    cmd += " --s3_bucket_name " + s3_bucket_name
-    cmd += ' --aws_profile ' + aws_profile
+cmd = gen_comm(zrange[0], zrange[1])
 cmd += ' --create_resources '
-print(cmd)
+print('\n' + cmd + '\n')
 
 for worker in range(workers):
-    start_z = worker * range_per_worker + zrange[0]
+    start_z = max((worker * range_per_worker +
+                   zrange[0]) // 16 * 16, zrange[0])
     if start_z < zrange[0]:
         start_z = zrange[0]
     if start_z > zrange[1]:
         # No point start a useless thread
         continue
-    end_z = min(zrange[1], (worker + 1) * range_per_worker + zrange[0])
 
-    cmd = "python " + script + " "
-    cmd += ' --base_path ' + shlex.quote(data_directory)
-    cmd += ' --base_filename ' + shlex.quote(file_name)
-    cmd += ' --extension ' + file_format
-    cmd += ' --datasource ' + source_type
-    cmd += ' --collection ' + collection
-    cmd += ' --experiment ' + experiment
-    cmd += ' --channel ' + channel
-    cmd += ' --voxel_size ' + voxel_size
-    cmd += ' --voxel_unit ' + voxel_unit
-    cmd += ' --datatype ' + data_type
-    cmd += ' --img_size ' + data_dimensions
-    cmd += ' --z_range %d %d ' % (start_z, end_z)
-    cmd += ' --warn_missing_files'
-    cmd += ' --boss_config_file ' + boss_config_file
-    cmd += ' --slack_token_file ' + slack_token
-    cmd += ' --z_step ' + z_step
+    next_z = ((worker + 1) * range_per_worker + zrange[0]) // 16 * 16
+    end_z = min(zrange[1], next_z)
 
-    cmd += " --slack_usr " + slack_username
-    if source_type == 's3':
-        cmd += " --s3_bucket_name " + s3_bucket_name
-        cmd += ' --aws_profile ' + aws_profile
-    cmd += " &"
-
+    cmd = gen_comm(start_z, end_z)
     print(cmd)
