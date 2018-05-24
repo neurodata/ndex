2ea1d671ce4ccf76741d183311bad774a279eab8
Ben Falk <falk.ben@gmail.com>
2018-05-24 16:56:41 -0400

    add split RGB channels functionality into script

--- scripts/expand_tiff_stacks.py
+++ scripts/expand_tiff_stacks.py
@@ -1,3 +1,7 @@
+'''
+Command line script to expand a tiff stack into separate TIFF files
+'''
+
 from pathlib import Path
 import argparse
 import os
@@ -5,19 +9,26 @@ import os
 import numpy as np
 import tifffile as tiff
 from tqdm import tqdm
-# from PIL import Image, ImageSequence
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Splits a tiff stack into separate tiff files')
 
-    parser.add_argument('--tiffstack', type=str,
-                        help='Full file path of tiff stack input')
-    parser.add_argument('--outpath', type=str,
-                        help='Full path for output files')
-    parser.add_argument('--datatype', type=str,
-                        help='Cast images as a particular dtype (uint8/uint16/uint64)')
+    parser.add_argument(
+        '--tiffstack', type=str, help='Full file path of tiff stack input')
+    parser.add_argument(
+        '--outpath', type=str, help='Full path for output files')
+    parser.add_argument(
+        '--datatype',
+        type=str,
+        help=
+        'Cast images as a particular dtype (uint8/uint16/uint64), note that data can be lost this way'
+    )
+    parser.add_argument(
+        '--split_RGB',
+        action='store_true',
+        help='Splits the RGB channels into separate folders')
     # parser.add_argument('--scale_to_datatype', action='store_true',
     #                     help='flag to scale the input datatype to the datatype specified')
 
@@ -32,14 +43,20 @@ def parse_args():
 
 def expand_stack(args):
     stackfile = Path(args.tiffstack)
+    assert stackfile.exists()
 
     if args.outpath is None:
         outname = stackfile.parent / stackfile.stem
     else:
         outname = Path(args.outpath)
-    if not outname.exists():
-        outname.mkdir()
+    outname.mkdir(exist_ok=True, parents=True)
 
+    if args.split_RGB:
+        # create RGB channel directories if they don't exist
+        channels = 'r', 'g', 'b'
+        [(outname / ch).mkdir(exist_ok=True) for ch in channels]
+
+    # reads the stack into memory (seems to be unavoidable with using tifffile library)
     stack = tiff.imread(str(stackfile))
 
     num_slices = len(stack)
@@ -47,10 +64,19 @@ def expand_stack(args):
     outfname = '{0:0{1}d}'
 
     for idx, page in tqdm(enumerate(stack), total=num_slices):
-        numpypage = np.array(page, dtype=args.datatype)
-        tiff.imsave(
-            str(outname / (outfname.format(idx, digits) + '.tif')),
-            data=numpypage)
+        if args.datatype:
+            page = page.astype(args.datatype)
+
+        if args.split_RGB:
+            for ch_idx, ch in enumerate(channels):
+                tiff.imsave(
+                    str(outname / ch /
+                        (outfname.format(idx, digits) + '.tif')),
+                    data=page[:, :, ch_idx])
+        else:
+            tiff.imsave(
+                str(outname / (outfname.format(idx, digits) + '.tif')),
+                data=page)
 
 
 def main():
