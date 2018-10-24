6a1143c1d872d49bc02477590d1c9806444c4466
Ben Falk <falk.ben@gmail.com>
2018-10-24 17:41:26 -0400

    nifti ingest script (start)
    update requirements

---
+++ scripts/ingest_nifti.py
@@ -0,0 +1,24 @@
+'''
+Command line script to load a nifti file and ingest it into the BOSS
+'''
+
+# load nifti file into memory
+
+# nifti data into numpy 3D / 4D data
+
+# extract timing information from nifti data?
+
+# create boss resources with timeseries support
+
+# update cutout function with timeseries support
+
+# iterate through numpy array (loop over each time segment 1 by 1)
+
+
+# argparse
+# nifti filename
+
+
+from pathlib import Path
+import argparse
+import nibabel as nib
